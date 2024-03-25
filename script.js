3caaf50c38d5e0e6f9b45eca7a81178ce1328368
Muhammadizhan <izhanayaz4@gmail.com>
2024-03-26 01:20:11 +0500

    some changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkDivisibility() {
-    var number = document.getElementById("inputNumber").value;
-    var resultMessage = document.getElementById("result");
+    var number = document.getElementById("inputNumber-1").value;
+    var resultMessage = document.getElementById("result-1");
 
     if (number % 3 === 0) {
         resultMessage.textContent = "Number is divisible by 3";
@@ -8,3 +8,34 @@ function checkDivisibility() {
         resultMessage.textContent = "Number is not divisible by 3";
     }
 }
+
+    // Question -02
+
+    document.getElementById("checkButton").addEventListener("click", function() {
+        var number = parseInt(document.getElementById("inputNumber-2").value);
+        var resultMessage = document.getElementById("result-2");
+    
+        if (number % 2 === 0) {
+            resultMessage.textContent = "The number is even";
+        } else {
+            resultMessage.textContent = "The number is odd";
+        }
+    });
+
+// Question -03
+
+document.getElementById("checkButton").addEventListener("click", function() {
+    var age = parseInt(document.getElementById("ageInput").value);
+    var resultMessage = document.getElementById("result");
+
+    if (age > 18) {
+        resultMessage.textContent = "Old enough";
+    } else {
+        resultMessage.textContent = "Too young";
+    }
+});
+
+
+
+
+    
