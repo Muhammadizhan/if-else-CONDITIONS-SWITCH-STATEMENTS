dd21c3c23a0f05dd8e167e296bc51c09ef7b17d4
Muhammadizhan <izhanayaz4@gmail.com>
2024-03-26 20:05:28 +0500

    updats

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-function checkDivisibility() {
-    var number = document.getElementById("inputNumber-1").value;
-    var resultMessage = document.getElementById("result-1");
+// function checkDivisibility() {
+//     var number = document.getElementById("inputNumber-1").value;
+//     var resultMessage = document.getElementById("result-1");
 
-    if (number % 3 === 0) {
-        resultMessage.textContent = "Number is divisible by 3";
-    } else {
-        resultMessage.textContent = "Number is not divisible by 3";
-    }
-}
+//     if (number % 3 === 0) {
+//         resultMessage.textContent = "Number is divisible by 3";
+//     } else {
+//         resultMessage.textContent = "Number is not divisible by 3";
+//     }
+// }
 
-    // Question -02
+//     // Question -02
 
-    document.getElementById("checkButton").addEventListener("click", function() {
-        var number = parseInt(document.getElementById("inputNumber-2").value);
-        var resultMessage = document.getElementById("result-2");
+//     document.getElementById("checkButton").addEventListener("click", function() {
+//         var number = parseInt(document.getElementById("inputNumber-2").value);
+//         var resultMessage = document.getElementById("result-2");
     
-        if (number % 2 === 0) {
-            resultMessage.textContent = "The number is even";
-        } else {
-            resultMessage.textContent = "The number is odd";
-        }
-    });
+//         if (number % 2 === 0) {
+//             resultMessage.textContent = "The number is even";
+//         } else {
+//             resultMessage.textContent = "The number is odd";
+//         }
+//     });
 
 // Question -03
 
-document.getElementById("checkButton").addEventListener("click", function() {
-    var age = parseInt(document.getElementById("ageInput").value);
-    var resultMessage = document.getElementById("result");
+// document.getElementById("checkButton").addEventListener("click", function() {
+//     var age = parseInt(document.getElementById("ageInput").value);
+//     var resultMessage = document.getElementById("result");
 
-    if (age > 18) {
-        resultMessage.textContent = "Old enough";
-    } else {
-        resultMessage.textContent = "Too young";
-    }
-});
+//     if (age > 18) {
+//         resultMessage.textContent = "Old enough";
+//     } else {
+//         resultMessage.textContent = "Too young";
+//     }
+// });
 
+// Question -04
+
+// function checkName() {
+//     var name = document.getElementById("nameInput").value;
+  
+//     if (name === "IzhanAyaz") {
+
+//       alert("Hello, it's you!");
+//     }
+//   }
+
+  //Question -05
+
+  
+  
 
 
 
